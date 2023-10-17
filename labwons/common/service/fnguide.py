1a9003a7c3898d361f1e27468b5c3a30a91679a3
JEHYEUK-LEE <jhlee_0319@naver.com>
2023-10-17 23:05:52 +0900

    Update

--- labwons/common/service/fnguide.py
+++ labwons/common/service/fnguide.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup as Soup
 from pykrx.stock import get_market_cap_by_date
 from urllib.request import urlopen
 import pandas as pd
+import numpy as np
 import requests, json
 
 
@@ -81,6 +82,20 @@ class fnguide(object):
         cap = self.cap.copy()
         cap.index = cap.index[:-1].tolist() + [data.index[-1]]
         return data.join(other=cap, how='left')[cap.columns.tolist() + data.columns.tolist()].astype(float)
+
+    def _multipleBand(self, header:str) -> pd.DataFrame:
+        if not hasattr(self, '_band_src'):
+            url = f"http://cdn.fnguide.com/SVO2/json/chart/01_06/chart_A{self._t}_D.json"
+            src = json.loads(urlopen(url=url).read().decode('utf-8-sig', 'replace'))
+            self.__setattr__('_band_src', src)
+        src = self.__getattribute__('_band_src')
+        header = pd.DataFrame(src[header])[['ID', 'NAME']].set_index(keys='ID')
+        header = header.to_dict()['NAME']
+        header.update({'GS_YM': '날짜'})
+        data = pd.DataFrame(src['CHART'])
+        data = data[header.keys()].replace('-', np.nan).replace('', np.nan)
+        data['GS_YM'] = pd.to_datetime(data['GS_YM'])
+        return data.rename(columns=header).set_index(keys='날짜').astype(float)
 
     @staticmethod
     def _finance(data:pd.DataFrame) -> pd.DataFrame:
@@ -237,7 +252,136 @@ class fnguide(object):
         index = data[data.columns[0]].tolist()
         return self._finance(data.iloc[index.index('수익성비율') + 1: ])
 
+    @property
+    def foreignRate(self) -> pd.DataFrame:
+        """
+        외국인보유비중(시계열) : 기간별 사용 시 .dropna() 사용
+        :return: 
+                                 3M               1Y               3Y
+                        종가   비중      종가   비중      종가   비중
+        날짜                                                           
+        2020-10-01       NaN    NaN       NaN    NaN  117090.0  42.69
+        2020-11-01       NaN    NaN       NaN    NaN  124662.0  42.62
+        2020-12-01       NaN    NaN       NaN    NaN  125610.0  42.78
+        ...              ...    ...       ...    ...       ...    ...
+        2023-10-12  158000.0  36.86       NaN    NaN       NaN    NaN
+        2023-10-13  157300.0  36.99       NaN    NaN       NaN    NaN
+        2023-10-16  156800.0  37.07  155780.0  36.91       NaN    NaN        
+        """
+        objs = dict()
+        for dt in ['3M', '1Y', '3Y']:
+            url = f"http://cdn.fnguide.com/SVO2/json/chart/01_01/chart_A{self._t}_{dt}.json"
+            data = json.loads(urlopen(url=url).read().decode('utf-8-sig', 'replace'))
+            frm = pd.DataFrame(data["CHART"])[['TRD_DT', 'J_PRC', 'FRG_RT']]
+            frm = frm.rename(columns={'TRD_DT': '날짜', 'J_PRC': '종가', 'FRG_RT': '비중'}).set_index(keys='날짜')
+            frm.index = pd.to_datetime(frm.index)
+            frm = frm.replace('', '0.0')
+            objs[dt] = frm
+        return pd.concat(objs=objs, axis=1).astype(float)
+    
+    @property
+    def consensus(self) -> pd.DataFrame:
+        """
+        컨센서스 (발행 의견의 전체 평균)
+        :return:
+                   투자의견  컨센서스    종가   격차
+        날짜
+        2022-10-17    4.00     180000  139900 -22.28
+        2022-10-18    4.00     180000  139700 -22.39
+        2022-10-19    4.00     180000  135800 -24.56
+        ...            ...        ...     ...    ...
+        2023-10-12    4.00     198000  158000 -20.20
+        2023-10-13    4.00     198000  157300 -20.56
+        2023-10-16    4.00     198333  156800 -20.94
+        """
+        url = f"http://cdn.fnguide.com/SVO2/json/chart/01_02/chart_A{self._t}.json"
+        raw = json.loads(urlopen(url=url).read().decode('utf-8-sig', 'replace'))
+        basis = pd.DataFrame(raw['CHART'])
+        basis = basis.rename(columns={'TRD_DT': '날짜', 'VAL1': '투자의견', 'VAL2': '컨센서스', 'VAL3': '종가'})
+        basis = basis.set_index(keys='날짜')
+        basis.index = pd.to_datetime(basis.index)
+        basis['컨센서스'] = basis['컨센서스'].apply(lambda x: int(x) if x else np.nan)
+        basis['격차'] = round(100 * (basis['종가'].astype(int) / basis['컨센서스'] - 1), 2)
+        return basis
 
+    @property
+    def perBand(self) -> pd.DataFrame:
+        """
+        PER 밴드
+        :return:
+                   수정주가    12.85X     19.44X     26.04X     32.63X     39.22X
+        날짜
+        2018-12-01  47050.0  41007.43   62045.68   83083.93  104122.18  125160.43
+        2019-01-01  50000.0  41298.85   62486.61   83674.36  104862.12  126049.88
+        2019-02-01  52800.0  41590.27   62927.53   84264.79  105602.06  126939.32
+        ...             ...       ...        ...        ...        ...        ...
+        2025-10-01      NaN  96413.81  145877.47  195341.13  244804.79  294268.44
+        2025-11-01      NaN  96413.81  145877.47  195341.13  244804.79  294268.44
+        2025-12-01      NaN  96413.81  145877.47  195341.13  244804.79  294268.44
+        """
+        return self._multipleBand('CHART_E')
+
+    @property
+    def pbrBand(self) -> pd.DataFrame:
+        """
+        PBR 밴드
+        :return:
+                   수정주가     2.24X      3.61X      4.98X      6.34X      7.71X
+        날짜
+        2018-12-01  47050.0  39169.38   63081.94   86994.49  110907.05  134819.60
+        2019-01-01  50000.0  39610.24   63791.93   87973.63  112155.33  136337.02
+        2019-02-01  52800.0  40051.10   64501.93   88952.77  113403.60  137854.44
+        ...             ...       ...        ...        ...        ...        ...
+        2025-10-01      NaN  72823.52  117281.63  161739.74  206197.85  250655.96
+        2025-11-01      NaN  72823.52  117281.63  161739.74  206197.85  250655.96
+        2025-12-01      NaN  72823.52  117281.63  161739.74  206197.85  250655.96
+        """
+        return self._multipleBand('CHART_B')
+
+    @property
+    def shortRatio(self) -> pd.DataFrame:
+        """
+        차입공매도 비중
+        :return:
+                    공매도비중      종가
+        날짜
+        2022-10-17       10.81  139900.0
+        2022-10-24        7.94  140000.0
+        2022-10-31        2.42  136800.0
+        ...                ...
+        2023-10-02       11.54  153800.0
+        2023-10-09       14.76  154700.0
+        2023-10-16       18.95  156800.0
+        """
+        url = f"http://cdn.fnguide.com/SVO2/json/chart/11_01/chart_A{self._t}_SELL1Y.json"
+        data = json.loads(urlopen(url=url).read().decode('utf-8-sig', 'replace'))
+        data = pd.DataFrame(data['CHART']).rename(
+            columns={'TRD_DT': '날짜', 'VAL': '공매도비중', 'ADJ_PRC': '종가'}
+        ).set_index(keys='날짜')
+        data.index = pd.to_datetime(data.index)
+        return data.astype(float)
+
+    @property
+    def shortBalance(self) -> pd.DataFrame:
+        """
+        대차잔고 비중
+        :return:
+                   대차잔고비중   종가
+        날짜
+        2022-10-17        3.50  139900
+        2022-10-24        3.50  140000
+        2022-10-31        3.44  136800
+        ...                ...     ...
+        2023-10-02        9.45  153800
+        2023-10-09        9.52  154700
+        2023-10-16        8.69  156800
+        """
+        url = f"http://cdn.fnguide.com/SVO2/json/chart/11_01/chart_A{self._t}_BALANCE1Y.json"
+        columns = {'TRD_DT': '날짜', 'BALANCE_RT': '대차잔고비중', 'ADJ_PRC': '종가'}
+        data = json.loads(urlopen(url=url).read().decode('utf-8-sig', 'replace'))
+        data = pd.DataFrame(data['CHART']).rename(columns=columns)[columns.values()].set_index(keys='날짜')
+        data.index = pd.to_datetime(data.index)
+        return data.astype(float)
 
 
 if __name__ == "__main__":
@@ -258,10 +402,16 @@ if __name__ == "__main__":
     # print(guide.annualCashFlow)
     # print(guide.annualGrowthRate)
     # print(guide.annualProfitRate)
-    print(guide.annualMultiples)
+    # print(guide.annualMultiples)
     # print(guide.quarterOverview)
     # print(guide.quarterProfitLoss)
     # print(guide.quarterAsset)
     # print(guide.quarterCashFlow)
     # print(guide.quarterGrowthRate)
     # print(guide.quarterProfitRate)
+    # print(guide.foreignRate)
+    # print(guide.consensus)
+    # print(guide.perBand)
+    # print(guide.pbrBand)
+    print(guide.shortRatio)
+    print(guide.shortBalance)
